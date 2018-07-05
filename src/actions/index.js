5a360ae317015b45631514f7c6f331b551e287e0
Sebastiaan van Kuyk <sebastiaan.vankuyk@gmail.com>
2018-07-05 15:53:24 -0500

    middleware added

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,12 +2,17 @@ import flats from '../flats';
 
 export function setFlats() {
 
-  return {
-    type: 'SET_FLATS',
-    payload: flats
-  }
-}
+  // call API, fetch JSON.
 
+  return fetch('https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json')
+    .then(response => response.json())
+    .then((data) => {
+      return {
+        type: 'SET_FLATS',
+        payload: data
+      }
+    });
+}
 export function selectFlat(flat) {
   return {
     type: 'SELECT_FLAT',
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { logger } from 'redux-logger';
+import reduxPromise from 'redux-promise';
 
 import '../assets/stylesheets/application.scss';
 
@@ -12,10 +14,13 @@ const reducers = combineReducers({
   selectedFlat: selectedFlat
 });
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middlewares = composeEnhancers(applyMiddleware(logger, reduxPromise));
+
 import App from './components/App.jsx';
 
 const root = document.getElementById('root');
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={createStore(reducers, {},  middlewares)}>
     <App/>
   </Provider>, root);
